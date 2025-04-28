56751295f5856486764bef0fdfd31f6aa45d8662
Onebluesky882 <onebluesky882@outlook.com>
2025-04-28 19:22:05 +0700

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,10 @@ import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
-    <div>
-      {" "}
+    <div className="grid grid-cols-5">
+      <div className="col-span-1">logo</div>
+      <div className="col-span-3">search </div>
+      <div className="col-span-1">button singin up </div>
       <ModeToggle />
     </div>
   );
